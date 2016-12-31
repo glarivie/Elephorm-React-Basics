dbd3178e76aba0ab6fb8f2915cbf3f9d3cce96d0
Guillaume LARIVIERE <glarivie@e3r2p22.42.fr>
2016-12-31 14:36:54 +0100

    step 2

--- src/constants/tweets.js
+++ src/constants/tweets.js
@@ -1,15 +1,21 @@
 const tweets = [{
+  avatar: 'https://pbs.twimg.com/profile_images/553711083064541184/9VsY9i09_bigger.jpeg',
   username: 'dan_abramov',
   fullname: 'Dan Abramov',
   content: 'Now, this is a configuration that makes sense to me. Specify browsers once, let #Babel and #Autoprefixer do the work. https://github.com/facebookincubator/create-react-app/pull/1249',
+  date: '12/12/2016',
 }, {
+  avatar: 'https://pbs.twimg.com/profile_images/669569370134290432/04TXkEbC_bigger.png',
   username: 'elephorm',
   fullname: 'Elephorm',
   content: 'Formez-vous dès maintenant au logiciel de montage vidéo accessible à tous Premiere Elements 15 ! #adobe http://ow.ly/2ZD9307uItG',
+  date: '11/12/2016',
 }, {
+  avatar: 'https://pbs.twimg.com/profile_images/778923731846590464/jVirtTZ1_bigger.jpg',
   username: 'hqro',
   fullname: 'Guillaume Larivière',
   content: 'Retrouvez-moi aujourd\'hui pour un petit tuto #React sur la plateforme @elephorm',
+  date: '11/12/2016',
 }];
 
 export default tweets;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
 import './Home.css';
+import { tweets } from '../../constants';
 
 const Home = () => (
   <div className="homepage">
-    <h1>HOMEPAGE</h1>
+    {tweets.map(({ username, fullname, content, avatar, date }, index) => (
+      <div className="tweet" key={index}>
+        <div className="avatar">
+          <img src={avatar} role="presentation" alt="avatar" />
+        </div>
+        <div className="data">
+          <div className="infos">
+            <strong className="fullname">{fullname}</strong>
+            <small className="gray">@{username}</small>
+            <small className="gray date">{date}</small>
+          </div>
+          <div className="content">
+            <p>{content}</p>
+          </div>
+        </div>
+      </div>
+    ))}
   </div>
 );
 
